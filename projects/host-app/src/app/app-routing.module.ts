b98254d1abcc6296117e440d4e51ee5ceefbb481
DESKTOP-AG49DLA\pc_27_05_2023 <lenalenina5677@gmail.com>
2023-12-15 14:17:04 +0300

    adding observable to get routes json file

--- projects/host-app/src/app/app-routing.module.ts
+++ projects/host-app/src/app/app-routing.module.ts
@@ -1,45 +1,57 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
-import { ConfigService } from './config.service';
-
+import { ConfigService } from './services/config.service';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
 ];
 
+export function initializeRoutes(router: Router, configService: ConfigService) {
+  return (): Observable<any> => {
+    return configService.getConfig()
+    .pipe(
+      switchMap((routesJson) => {
+        if (!Array.isArray(routesJson)) {
+          console.error('routesJson is not an array');
+          return [];
+        }
+
+        let dynamicRoutes = routesJson.map((route) => ({
+          path: route.path,
+          loadChildren: () =>
+            loadRemoteModule({
+              remoteEntry: route.remoteEntry,
+              remoteName: route.remoteName,
+              exposedModule: route.exposedModule,
+            })
+              .then((m) => m[route.module])
+              .catch((err) => console.log(err)),
+        }));
+
+        router.resetConfig([...routes, ...dynamicRoutes]);
+        return [];
+      })
+    );
+  };
+}
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeRoutes,
+      multi: true,
+      deps: [Router, ConfigService]
+    }
+  ]
 })
 export class AppRoutingModule {
-
-  constructor(private router: Router, private configService: ConfigService) {
-    this.loadRoutes();
-  }
-
-  loadRoutes() {
-    this.configService.getConfig().subscribe((routesJson) => {
-      if (!Array.isArray(routesJson)) {
-        console.error('routesJson is not an array');
-        return;
-      }
-
-      let dynamicRoutes = routesJson.map((route) => ({
-        path: route.path,
-        loadChildren: () =>
-          loadRemoteModule({
-            remoteEntry: route.remoteEntry,
-            remoteName: route.remoteName,
-            exposedModule: route.exposedModule,
-          })
-            .then((m) => m[route.module])
-            .catch((err) => console.log(err)),
-      }));
-
-      this.router.resetConfig([...routes, ...dynamicRoutes]);
-    });
-  }
+  constructor() { }
 }
