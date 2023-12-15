32ccdc8ec8185b8489f355d7df5a2f8fe7e525f8
DESKTOP-AG49DLA\pc_27_05_2023 <lenalenina5677@gmail.com>
2023-12-15 13:05:53 +0300

    adding config service

--- projects/host-app/src/app/app-routing.module.ts
+++ projects/host-app/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
-import AppRoutingJson from '../assets/routes-config.json';
+import { ConfigService } from './config.service';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -15,31 +16,30 @@ const routes: Routes = [
 })
 export class AppRoutingModule {
 
-  constructor(private router: Router) {
-    this.prepareRoutes(AppRoutingJson);
+  constructor(private router: Router, private configService: ConfigService) {
+    this.loadRoutes();
   }
 
-  prepareRoutes(routesJson: any) {
-    if (!Array.isArray(routesJson)) {
-      console.error("routesJson is not an array");
-      return;
-    }
+  loadRoutes() {
+    this.configService.getConfig().subscribe((routesJson) => {
+      if (!Array.isArray(routesJson)) {
+        console.error('routesJson is not an array');
+        return;
+      }
 
-    let routesArr = <any[]>routesJson;
-
-    routesArr.forEach(route => {
-      routes.push({
+      let dynamicRoutes = routesJson.map((route) => ({
         path: route.path,
-        loadChildren: () => {
-          return loadRemoteModule({
+        loadChildren: () =>
+          loadRemoteModule({
             remoteEntry: route.remoteEntry,
             remoteName: route.remoteName,
             exposedModule: route.exposedModule,
-          }).then(m => m[route.module]).catch(err => console.log(err));
-        }
-      });
-    });
+          })
+            .then((m) => m[route.module])
+            .catch((err) => console.log(err)),
+      }));
 
-    this.router.resetConfig(routes);
+      this.router.resetConfig([...routes, ...dynamicRoutes]);
+    });
   }
 }
